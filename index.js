d6c0d605e081e48625a6b76da49b4559615cbdae
ramiinour <raminourgunz@gmail.com>
2022-04-14 05:07:20 +0300

    exercises 3.19 to 3.21 expanding validation and re-deploying to heroku

--- index.js
+++ index.js
@@ -22,29 +22,6 @@ app.use(
 
 app.use(express.static("build"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1>Hellow World!</h1>");
 });
@@ -99,7 +76,11 @@ app.post("/api/persons", (request, response, next) => {
 app.put("/api/persons/:id", (request, response, next) => {
   const body = request.body;
 
-  Person.findByIdAndUpdate(request.params.id, body, { new: true })
+  Person.findByIdAndUpdate(request.params.id, body, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedPerson) => response.json(updatedPerson))
     .catch((error) => next(error));
 });
@@ -115,6 +96,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
--- models/person.js
+++ models/person.js
@@ -14,8 +14,22 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^(\d{2}|\d{3})-\d+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    minLength: 8,
+    required: true,
+  },
 });
 
 personSchema.set("toJSON", {
